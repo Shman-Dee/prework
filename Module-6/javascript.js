32199f06d31d355e6babe8b2aa3d78c0447cc879
Andrew McInally <realfunnyestate@yahoo.com>
2021-07-19 21:25:08 -0600

    my prework for bootcamp

---
+++ Module-6/javascript.js
@@ -0,0 +1,25 @@
+
+
+function growpicture(){
+
+  document.getElementById("box").style="height:450px; width:450px; background-color:orange; margin:25px";
+
+}
+
+function bluepicture(){
+
+  document.getElementById("box").style.background = "blue";
+
+}
+
+function fadepicture(){
+
+  document.getElementById("box").style.opacity = "0.5";
+
+}
+
+function resetpicture(){
+
+  document.getElementById("box").style="height:150px; width:150px; background-color:orange; margin:25px";
+
+}
